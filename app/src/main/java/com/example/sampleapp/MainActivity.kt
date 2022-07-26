5d376cdb764510c7b83ca17e71bc4951d2b0d2c3
Dyno <dvghule121@gmail.com>
2022-07-26 16:17:45 +0530

    initial commit

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botttomNavigationVIew = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+        botttomNavigationVIew.setupWithNavController(navController)
+
+    }
+}
